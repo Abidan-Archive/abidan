9d7da8cee17df6dfe9d0fcfce87e8113fe4a8685
Michael Manning <michael@manning390.com>
2025-01-31 00:17:01 -0800

    See if this improves recaptcha performance for page load

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <script src="https://www.google.com/recaptcha/api.js?render={{config('services.recaptcha_v3.sitekey')}}"></script>
     <script>
         window.grecaptcha_sitekey = "{{config('services.recaptcha_v3.sitekey')}}";
     </script>
+    <script defer src="https://www.google.com/recaptcha/api.js?render={{config('services.recaptcha_v3.sitekey')}}"></script>
     @include('favicons')
     @vite('resources/js/app.js')
     @inertiaHead
